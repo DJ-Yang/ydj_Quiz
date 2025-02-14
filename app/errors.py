5b245600ffeef3a0dfdda11510959e1229fb337e
양동진 <dong3831@hanmail.net>
2025-02-14 14:57:16 +0900

    delete unnecessary error

--- app/errors.py
+++ app/errors.py
@@ -29,26 +29,3 @@ class ValidationError(APIException):
     def __init__(self, message: str) -> None:
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, message=message)
 
-
-class MatchImpossibleError(APIException):
-    def __init__(self) -> None:
-        super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            message="이미 매칭이 진행중입니다."
-        )
-
-
-class NoMatchingUsersError(APIException):
-    def __init__(self) -> None:
-        super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            message="해당 지역에 매칭 가능한 유저가 존재하지 않습니다."
-        )
-
-
-class NoLocationError(APIException):
-    def __init__(self) -> None:
-        super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            message="매칭을 위해서는 위치 정보를 먼저 등록해주세요."
-        )
